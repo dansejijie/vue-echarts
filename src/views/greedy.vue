e25520867f6e2e37b26140da0fbd18f0ff1be225
郑强 <zhengqiang@gomrwind.com>
2023-04-26 20:38:56 +0800

    feat: [greedy] 废弃 loss & 利润精度

--- src/views/greedy.vue
+++ src/views/greedy.vue
@@ -2,7 +2,6 @@
   <div class="fill-container">
     <div id="order"></div>
     <div id="profit"></div>
-    <div id="loss"></div>
     <div id="loss-num"></div>
   </div>
 </template>
@@ -21,7 +20,6 @@ export default {
     this.fetchData();
   },
   methods: {
-
     drawOption(option, ele) {
       let myChart = this.$echarts(ele);
       myChart.clear();
@@ -29,25 +27,31 @@ export default {
       myChart.setOption(option);
     },
 
-    drawOrder({orderList  = [], priceList = []}) {
+    drawOrder({ orderList = [], priceList = [] }) {
       const markList = [];
       console.log('orderList', orderList);
-      orderList.forEach(order=>{
+      orderList.forEach((order) => {
         const unix = dayjs(order.createdAt).unix() * 1000;
-        const ratio = (((order.sellPrice - order.price )/order.price)*100).toFixed(1) + '%';
+        const ratio = (((order.sellPrice - order.price) / order.price) * 100).toFixed(1) + '%';
         const itemStyle = {};
-        if(order.side === 'buy') {
+        if (order.side === 'buy') {
           itemStyle.color = 'red';
         } else {
           itemStyle.color = 'green';
           itemStyle.opacity = 0.6;
         }
-        if(order.sellOrderId) {
+        if (order.sellOrderId) {
           itemStyle.borderColor = 'yellow';
         }
-        markList.push({ value: ratio, xAxis: unix, yAxis: order.price, itemStyle, label: {color: '#fff'} }); 
+        markList.push({
+          value: ratio,
+          xAxis: unix,
+          yAxis: order.price,
+          itemStyle,
+          label: { color: '#fff' },
+        });
       });
-      
+
       const option = {
         title: {
           text: '订单',
@@ -62,9 +66,7 @@ export default {
         xAxis: {
           type: 'time',
         },
-        yAxis: [
-          { type: 'value', min: 'dataMin', max: 'dataMax'}, 
-        ],
+        yAxis: [{ type: 'value', min: 'dataMin', max: 'dataMax' }],
         series: [
           {
             name: '价格',
@@ -77,31 +79,33 @@ export default {
         ],
       };
       const ele = document.querySelector('#order');
-      this.drawOption(option, ele)
+      this.drawOption(option, ele);
     },
 
-    async drawProfit({orderList  = []}) {
+    async drawProfit({ orderList = [] }) {
       const idMap = {};
-      orderList.forEach(order=>{
+      orderList.forEach((order) => {
         idMap[order._id] = order;
       });
       let profitList = [];
       let left = 0;
-      let right  = 0;
-      orderList.filter(order=>order.side === 'buy').forEach(order=>{
-        const unix = dayjs(order.createdAt).unix() * 1000;
-          if(order.sellOrderId) {
+      let right = 0;
+      orderList
+        .filter((order) => order.side === 'buy')
+        .forEach((order) => {
+          if (order.sellOrderId) {
+            const unix = dayjs(order.createdAt).unix() * 1000;
             const sellOrder = idMap[order.sellOrderId];
             left = left + order.amount - sellOrder.amount;
             right = right - order.amount * order.price + sellOrder.amount * sellOrder.price;
-            const profit = left  + right / sellOrder.price;
-            profitList.push([unix, profit])
+            const profit = Number((left + right / sellOrder.price).toFixed(4));
+            profitList.push([unix, profit]);
           }
-      });
+        });
 
       const option = {
         title: {
-          text: '已对冲币本位累计',
+          text: '已对冲BTC币本位累计',
           left: 'center',
         },
         tooltip: {
@@ -113,7 +117,7 @@ export default {
         xAxis: {
           type: 'time',
         },
-        yAxis: { type: 'value', min: 'dataMin', max: 'dataMax'}, 
+        yAxis: { type: 'value', min: 'dataMin', max: 'dataMax' },
         series: [
           {
             name: '利润',
@@ -124,63 +128,20 @@ export default {
       };
       const ele = document.querySelector('#profit');
       console.log('profit', option, ele);
-      this.drawOption(option, ele)
-    },
-    async drawLoss({orderList  = []}) {
-      const idMap = {};
-      orderList.forEach(order=>{
-        idMap[order._id] = order;
-      });
-      let lossList = [];
-      let left = 0;
-      let right  = 0;
-      orderList.filter(order=>order.side === 'buy' && !order.sellOrderId).forEach(order=>{
-        const unix = dayjs(order.createdAt).unix() * 1000;
-        left = left + order.amount;
-        right = right - order.amount * order.price;
-        const loss = left * order.price + right;
-        lossList.push([unix, loss])
-      });
-
-      const option = {
-        title: {
-          text: '未对冲订单',
-          left: 'center',
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            animation: false,
-          },
-        },
-        xAxis: {
-          type: 'time',
-        },
-        yAxis: { type: 'value', min: 'dataMin', max: 'dataMax'}, 
-        series: [
-          {
-            name: '利润',
-            data: lossList,
-            type: 'line',
-          },
-        ],
-      };
-      const ele = document.querySelector('#loss');
-      console.log('loss', option, ele);
-      this.drawOption(option, ele)
+      this.drawOption(option, ele);
     },
 
-    async drawLossOrder({orderList  = []}) {
+    async drawLossOrder({ orderList = [] }) {
       let numList = [];
       let num = 0;
-      orderList.forEach(order=>{
+      orderList.forEach((order) => {
         const unix = dayjs(order.createdAt).unix() * 1000;
-        if(order.side === 'buy') {
-          num ++;
+        if (order.side === 'buy') {
+          num++;
         } else {
-          num --;
+          num--;
         }
-        numList.push([unix, num])
+        numList.push([unix, num]);
       });
 
       const option = {
@@ -197,7 +158,7 @@ export default {
         xAxis: {
           type: 'time',
         },
-        yAxis: { type: 'value', min: 'dataMin', max: 'dataMax'}, 
+        yAxis: { type: 'value', min: 'dataMin', max: 'dataMax' },
         series: [
           {
             name: '未对冲订单数',
@@ -208,10 +169,12 @@ export default {
       };
       const ele = document.querySelector('#loss-num');
       console.log('loss', option, ele);
-      this.drawOption(option, ele)
+      this.drawOption(option, ele);
     },
     async fetchData() {
-      const res = await axios.get('/task/market/order-profit', {params:{ orderModel: 'GreedyOrder'}});
+      const res = await axios.get('/task/market/order-profit', {
+        params: { orderModel: 'GreedyOrder' },
+      });
 
       this.drawOrder(res.data.data);
       this.drawProfit(res.data.data);
@@ -226,7 +189,7 @@ export default {
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
-  >div {
+  > div {
     flex: 1;
     min-height: 40vh;
     max-height: 70vh;
