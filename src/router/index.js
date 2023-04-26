c268c746695af1294a626d09e7fd8918127f5d6c
郑强 <zhengqiang@gomrwind.com>
2023-04-26 20:38:13 +0800

    feat: [pageX] 废弃无用页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,52 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 import home from '@/views/home';
 
-Vue.use(Router)
+Vue.use(Router);
 
 const router = new Router({
-    routes: [
+  routes: [
+    {
+      path: '/',
+      redirect: '/cex-dex',
+    },
+    {
+      path: '',
+      name: 'home',
+      component: home,
+      children: [
         {
-            path: '/',
-            redirect: '/cex-dex'
+          path: '/cex-dex',
+          name: 'cex-dex',
+          component: () => import('@/views/cex-dex'),
         },
         {
-            path: '',
-            name: 'home',
-            component: home,
-            children: [
-                {
-                    path: '/cex-dex',
-                    name: 'cex-dex',
-                    component: () => import('@/views/cex-dex')
-                },
-                {
-                  path: '/order-profit',
-                  name: 'order-profit',
-                  component: () => import('@/views/order-profit')
-              },
-              {
-                path: '/greedy',
-                name: 'greedy',
-                component: () => import('@/views/greedy')
-            },
-                {
-                  path: '/grid',
-                  name: 'grid',
-                  component: () => import('@/views/grid')
-                },
-                {
-                  path: '/cow-bear',
-                  name: 'cow-bear',
-                  component: () => import('@/views/cow-bear')
-              },
-              {
-                path: '/cow-bear-stage',
-                name: 'cow-bear-stage',
-                component: () => import('@/views/cow-bear-stage')
-            },
-                {
-                    path: '/page2',
-                    name: 'page2',
-                    component: () => import('@/views/page2')
-                },
-                {
-                    path: '/page3',
-                    name: 'page3',
-                    component: () => import('@/views/page3')
-                }
-            ]
-        }
-    ]
-})
-export default router
+          path: '/order-profit',
+          name: 'order-profit',
+          component: () => import('@/views/order-profit'),
+        },
+        {
+          path: '/greedy',
+          name: 'greedy',
+          component: () => import('@/views/greedy'),
+        },
+        {
+          path: '/grid',
+          name: 'grid',
+          component: () => import('@/views/grid'),
+        },
+        {
+          path: '/cow-bear',
+          name: 'cow-bear',
+          component: () => import('@/views/cow-bear'),
+        },
+        {
+          path: '/cow-bear-stage',
+          name: 'cow-bear-stage',
+          component: () => import('@/views/cow-bear-stage'),
+        },
+      ],
+    },
+  ],
+});
+export default router;
