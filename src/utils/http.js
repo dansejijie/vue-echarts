86b833cc79d5a960af96457085b2d8b9cea9edea
郑强 <zhengqiang@gomrwind.com>
2023-07-15 12:09:14 +0800

    feat: [配置] 环境变量

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,3 +1,11 @@
 import axios from "axios";
 
-export default axios.create({baseURL: 'http://localhost:3201'})
+console.log('process', process, process.env)
+const { VUE_APP_API_ENV } = process.env;
+let baseURL = '';
+if(VUE_APP_API_ENV === 'pro') {
+  baseURL= 'http://localhost:3201';
+} else {
+  baseURL= 'http://localhost:3200';
+}
+export default axios.create({baseURL})
